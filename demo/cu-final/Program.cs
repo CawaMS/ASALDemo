af910781d67f2ad85612bb2c9c71dd3e9e9bacb1
Catherine Wang <cawa@microsoft.com>
2017-10-25 12:47:09 -0700

    demo project and VS extensions

---
+++ demo/cu-final/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((ctx, builder) =>
+             {
+                 var keyVaultEndpoint = GetKeyVaultEndpoint();
+                 if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                 {
+                     var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                     var keyVaultClient = new KeyVaultClient(
+                         new KeyVaultClient.AuthenticationCallback(
+                             azureServiceTokenProvider.KeyVaultTokenCallback));
+                     builder.AddAzureKeyVault(
+                         keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                 }
+             })
+
+                .UseStartup<Startup>()
+                .Build();
+
+        private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+    }
+}
